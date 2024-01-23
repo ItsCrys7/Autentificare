a49db2c884e9b34443733ed36c43372bfecf598b
ItsCrys7 <cristian.toanca@emanuel.ro>
2024-01-22 17:24:56 -0800

    Autentificare

---
+++ Autentiicare/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Autentificare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usernamecorect = "Crys";
+            const string passwordcorect = "123crys";
+
+            bool autentifacare = false;
+
+            while (!autentifacare)
+            {
+                Console.WriteLine("Introduceti username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Introduceti password: ");
+                string password = Console.ReadLine();
+                if (username == usernamecorect && password == passwordcorect)
+                {
+                    Console.WriteLine("Bine ai venit, " + usernamecorect);
+                    autentifacare = true;
+                }
+                else
+                {
+                    Console.WriteLine("Autentificarea nu a putut fi procesata. Va rugam incercati din nou. ");
+
+                }
+            }
+        }
+    }
+}
